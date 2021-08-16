008f32b133cb7313060c952ff98bf8e8c6520ca3
Lamdayy <89033286+Lamdayy@users.noreply.github.com>
2021-08-17 00:09:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
-const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+const textConfig = {
+  text1: "Hi,chào cậu :3",
+  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Cậu yêu tớ có phải không nào ._.",
+  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
+  text5: "Đéo",
+  text6: "Yêu Lâmm",
+  text7: "Cho tớ biết lí do cậu thích tớ đi :vv",
+  text8: "Gửi cho tớ :3",
+  text9: "Vì Lâm đẹp try vlllll",
+  text10: "Love u 3000",
+  text11:
+    "Khi nào hết dịch mình đi chơi nhaaa! Giờ thì nhắn tin cho tớ đi nào :3",
+  text12: "Okii lunn <3",
+};
+    messLink: 'https://m.me/100056127415236
 }
